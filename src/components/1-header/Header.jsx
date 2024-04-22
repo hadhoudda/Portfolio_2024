20e62c8fa4ede97a23d6833596f6681dfba267c6
Houda Ben Abdallah <ahhouda@yahoo.fr>
2024-04-22 23:56:24 +0200

    header responsi

--- src/components/1-header/Header.jsx
+++ src/components/1-header/Header.jsx
@@ -1,24 +1,50 @@
-import './header.scss';
+import { useState } from "react";
+import "./header.scss";
 
 function Header() {
+  const [toggleMenu, setToggleMenu] = useState(true);
+
+  const toggleNav = () => {
+    setToggleMenu(!toggleMenu);
+  };
   return (
-    <header className='style flex'>
-      <a href="#"><img className ="logo" src="./src/images/logo.png" alt='image__houda' /></a>
-      <nav>
-        <ul className='flex'>
-          <li><a href="#">Accueil</a></li>
-          <li><a href="#">À propos</a></li>
-          <li><a href="#">Compétences</a></li>
-          <li><a href="#">Projets</a></li>
-          <li><a href="#">Contact</a></li>
+    <header className="style flex">
+      <a className="link-logo" href="#">
+        <img className="logo" src="./src/images/logo.png" alt="image__houda" />
+      </a>
+      <nav className={toggleMenu ? "nav-desktop" : "nav-mobil"}>
+        <ul className={toggleMenu ? "flex" : "modal"}>
+          <li>
+            <a href="#">Accueil</a>
+          </li>
+          <li>
+            <a href="#">À propos</a>
+          </li>
+          <li>
+            <a href="#">Compétences</a>
+          </li>
+          <li>
+            <a href="#">Projets</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
         </ul>
+        <div onClick={toggleNav}>
+          <i
+            className={
+              toggleMenu ? "fa-solid fa-bars btn-menu" : "fa-solid fa-xmark btn-menu "
+            }
+          ></i>
+        </div>
       </nav>
-      <button>
+
+      <button className="btn-mode">
         <i className="fa-solid fa-sun icone-moon"></i>
-      {/* <i className="fa-regular fa-moon icone-moon"></i> */}
+        {/* <i className="fa-regular fa-moon icone-moon"></i> */}
       </button>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
