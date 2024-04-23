faeb2de0ebc4e9b51382a11ef2739ccb62ec7137
Houda Ben Abdallah <ahhouda@yahoo.fr>
2024-04-24 00:43:45 +0200

    added animation navbar

--- src/components/1-header/Header.jsx
+++ src/components/1-header/Header.jsx
@@ -2,47 +2,74 @@ import { useState } from "react";
 import "./header.scss";
 
 function Header() {
-  const [toggleMenu, setToggleMenu] = useState(true);
+  const [toggleMenu, setToggleMenu] = useState(false);
 
-  const toggleNav = () => {
-    setToggleMenu(!toggleMenu);
-  };
   return (
     <header className="style flex">
       <a className="link-logo" href="#">
         <img className="logo" src="./src/images/logo.png" alt="image__houda" />
       </a>
-      <nav className={toggleMenu ? "nav-desktop" : "nav-mobil"}>
-        <ul className={toggleMenu ? "flex" : "modal"}>
-          <li>
-            <a href="#">Accueil</a>
-          </li>
-          <li>
-            <a href="#">À propos</a>
-          </li>
-          <li>
-            <a href="#">Compétences</a>
-          </li>
-          <li>
-            <a href="#">Projets</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>
-          </li>
-        </ul>
-        <div onClick={toggleNav}>
-          <i
-            className={
-              toggleMenu ? "fa-solid fa-bars btn-menu" : "fa-solid fa-xmark btn-menu "
-            }
-          ></i>
-        </div>
-      </nav>
-
       <button className="btn-mode">
         <i className="fa-solid fa-sun icone-moon"></i>
         {/* <i className="fa-regular fa-moon icone-moon"></i> */}
       </button>
+      <nav>
+        <div className="nav-desktop">
+          <ul className="flex">
+            <li>
+              <a href="#">Accueil</a>
+            </li>
+            <li>
+              <a href="#">À propos</a>
+            </li>
+            <li>
+              <a href="#">Compétences</a>
+            </li>
+            <li>
+              <a href="#">Projets</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <div
+        onClick={() => {
+          setToggleMenu(true);
+        }}
+      >
+        <i  className={setToggleMenu ? "fa-solid fa-bars btn-menu": "div-none"}></i>
+      </div>
+
+      {toggleMenu && (
+        <div className="nav-mobil">
+          <ul className="modal">
+            <li>
+               <button onClick={() => { setToggleMenu(false)}}>
+               <i className="fa-solid fa-xmark btn-menu "></i>
+               </button>
+              
+            </li>
+            <li>
+              <a href="#">Accueil</a>
+            </li>
+            <li>
+              <a href="#">À propos</a>
+            </li>
+            <li>
+              <a href="#">Compétences</a>
+            </li>
+            <li>
+              <a href="#">Projets</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      )}
     </header>
   );
 }
