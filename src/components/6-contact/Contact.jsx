890d4471fffaccd10e5cb7b7827b6c65e735014c
Houda Ben Abdallah <ahhouda@yahoo.fr>
2024-05-08 21:35:25 +0200

    section send message

--- src/components/6-contact/Contact.jsx
+++ src/components/6-contact/Contact.jsx
@@ -1,7 +1,17 @@
+import {useState}  from "react";
+import { useForm, ValidationError } from "@formspree/react";
 import "./contact.scss";
 
 function Contact() {
-
+  const [state, handleSubmit] = useForm("xzbnwpzl");
+  const [isAlert, setAlert] = useState(false);
+  const handleAlert = () =>{
+        setAlert(true);
+        setTimeout(() => {
+            setAlert(false);}, 400);
+        }
+    
+  
   return (
     <section className="section-contact style flex" id="contact">
       <div className="form flex">
@@ -11,7 +21,7 @@ function Contact() {
         </p>
 
         {/* <form ref={form} onSubmit={sendEmail} className="form-contact "> */}
-        <form className="form-contact flex">
+        <form onSubmit={handleSubmit} className="form-contact flex">
           <input
             type="text"
             id="name"
@@ -19,38 +29,59 @@ function Contact() {
             name="user_name"
             required
           />
+
           <input
             type="email"
             id="email"
             placeholder="Votre email*"
-            name="user_email"
+            name="email"
             required
           />
-          <input type="text" placeholder="Sujet" name="subject" />
+          <ValidationError prefix="Email" field="email" errors={state.errors} />
           <textarea
             name="message"
             placeholder="Votre message*"
             id="message"
             required
-          ></textarea>
+          />
+          <ValidationError
+            prefix="Message"
+            field="message"
+            errors={state.errors}
+          />
           <div className="container-send">
             {/* {isAlert && <p className="message-confirm">message bien envoyé</p>} */}
             {/* <p className="message-confirm">message bien envoyé</p> */}
             {/* <button onClick={handleAlert} type="submit" className="btn-message"> */}
-            <button type="submit" className="btn-contact">
-              Envoyer
+            <button
+            onSubmit={handleAlert}
+              type="submit"
+              disabled={state.submitting}
+              className="btn-contact"
+            >
+              {state.submitting ? "Envoi..." : "Envoyer"}
+              
             </button>
+            { /* ======Message to send======== */ }
+
+            { state.succeeded && setAlert && (<p className="message-confirm">message bien envoyé  ✅ </p>)
+              
+            }
           </div>
           <p className="info-form">* Champ obligatoire</p>
         </form>
       </div>
 
-
       <div className="contact flex">
-        <a href="tel:+33622464454"><i className="fa-solid fa-phone"> </i>: 0622464454</a>
-        <a href="mailto:ahhouda@yahoo.fr"><i className="fa-solid fa-envelope"></i>: ahhouda@yahoo.fr</a>
-        <a href="https://www.linkedin.com/in/houda-ben-abdallah-developpeuse-web/"><i className="fa-brands fa-linkedin"></i>: Linkedin</a>
-        
+        <a href="tel:+33622464454">
+          <i className="fa-solid fa-phone"> </i>: 0622464454
+        </a>
+        <a href="mailto:ahhouda@yahoo.fr">
+          <i className="fa-solid fa-envelope"></i>: ahhouda@yahoo.fr
+        </a>
+        <a href="https://www.linkedin.com/in/houda-ben-abdallah-developpeuse-web/">
+          <i className="fa-brands fa-linkedin"></i>: Linkedin
+        </a>
       </div>
     </section>
   );
